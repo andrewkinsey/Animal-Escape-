85492d550d98952e1c46c2886f9df72506e7bc97
Andrew James Kinsey <akinsey@andrews-macbook-air-123.local>
2017-04-26 10:03:06 -0500

    moving biker

--- Animal Escape/Animals.swift
+++ Animal Escape/Animals.swift
@@ -9,8 +9,40 @@
 import UIKit
 import SpriteKit
 
-class Animals: SKSpriteNode
+class Animals: NSObject
 {
+    var animalNode = SKSpriteNode()
     var image = SKTexture(imageNamed: "")
+    var xPosition = Double()
+    var yPosition = Double()
+    var width = Double()
+    var height = Double()
+    
+    
+    init(Texture: SKTexture, xCoordinate: Double, yCoordinate: Double, Width: Double, Height: Double)
+    {
+       
+        image = Texture
+        xPosition = xCoordinate
+        yPosition = yCoordinate
+        width = Width
+        height = Height
+    }
+    
+    override init()
+    {
+        image = SKTexture(imageNamed: "Biker")
+        xPosition = 0
+        yPosition = 0
+        width = 10
+        height = 10
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    
+    
     
 }
--- Animal Escape/Level1Scene.swift
+++ Animal Escape/Level1Scene.swift
@@ -14,6 +14,8 @@ class Level1Scene: SKScene, SKPhysicsContactDelegate
     
     var wall: SKSpriteNode!
     var winZone: SKSpriteNode!
+    var animalNode: SKSpriteNode!
+    var bikerNode: SKSpriteNode!
     
     override func didMove(to view: SKView)
     {
@@ -21,11 +23,7 @@ class Level1Scene: SKScene, SKPhysicsContactDelegate
         physicsWorld.contactDelegate = self
         self.physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
         
-        makeWall(wallHeight: 800, wallWidth: 40, xPosition: 70, yPosition: 650)
-        makeWall(wallHeight: 40, wallWidth: Double(frame.width - 100), xPosition: Double(frame.width / 2), yPosition: 250)
-        makeWall(wallHeight: 40, wallWidth: Double(frame.width - 100), xPosition: Double(frame.width / 2), yPosition: 1050)
-        makeWall(wallHeight: 800, wallWidth: 40, xPosition: Double(frame.maxX - 70), yPosition: 650)
-        makeWinZone()
+        createGame()
     }
     
     
@@ -54,6 +52,57 @@ class Level1Scene: SKScene, SKPhysicsContactDelegate
         addChild(winZone)
     }
     
+    func makeAnimal(animalHeight: Double, animalWidth: Double, xPosition: Double, yPosition: Double, animalName: String, animalTexture: SKTexture, horizontalMovement: Bool)
+    {
+        animalNode = SKSpriteNode(texture: animalTexture)
+        animalNode.size = CGSize(width: animalWidth, height: animalHeight)
+        animalNode.position = CGPoint(x: xPosition, y: yPosition)
+        animalNode.zPosition = 5
+        animalNode.name = animalName
+        animalNode.physicsBody = SKPhysicsBody(rectangleOf: animalNode.size)
+        animalNode.physicsBody?.isDynamic = false
+        
+        
+        addChild(animalNode)
+    }
+    
+    func makeBiker()
+    {
+        bikerNode = SKSpriteNode(texture: SKTexture(imageNamed: "Biker"))
+        bikerNode.size = CGSize(width: 130, height: 125)
+        bikerNode.position = CGPoint(x: 150, y: 760)
+        bikerNode.zPosition = 5
+        bikerNode.name = "biker"
+        bikerNode.physicsBody = SKPhysicsBody(rectangleOf: bikerNode.size)
+        bikerNode.physicsBody?.isDynamic = false
+        
+        
+        addChild(bikerNode)
+    }
+    
+    func createGame()
+    {
+        makeWall(wallHeight: 800, wallWidth: 40, xPosition: 70, yPosition: 650)
+        makeWall(wallHeight: 40, wallWidth: Double(frame.width - 100), xPosition: Double(frame.width / 2), yPosition: 250)
+        makeWall(wallHeight: 40, wallWidth: Double(frame.width - 100), xPosition: Double(frame.width / 2), yPosition: 1050)
+        makeWall(wallHeight: 800, wallWidth: 40, xPosition: Double(frame.maxX - 70), yPosition: 650)
+        makeWinZone()
+        makeBiker()
+        //make animals
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        let touch = touches.first
+        if bikerNode.contains((touch?.location(in: self))!)
+        {
+            for touch in touches
+            {
+                let location = touch.location(in: self)
+                bikerNode.position.x = location.x
+            }
+        }
+    }
     
     
     
