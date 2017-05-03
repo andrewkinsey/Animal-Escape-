a1fa4d03807b39384663dc09054befef028250a5
Andrew James Kinsey <akinsey@andrews-macbook-air-123.local>
2017-05-03 10:35:59 -0500

    Working on collision detection

--- Animal Escape/LevelSelectScene.swift
+++ Animal Escape/LevelSelectScene.swift
@@ -28,13 +28,13 @@ class LevelSelectScene: SKScene
 
     func makeLevelNodes()
     {
-        makeLevelNode1(yPoint: Float(frame.maxY - 100))
-        makeLevelNode2(yPoint: Float(frame.maxY - 260))
-        makeLevelNode3(yPoint: Float(frame.maxY - 420))
-        makeLevelNode4(yPoint: Float(frame.maxY - 580))
-        makeLevelNode5(yPoint: Float(frame.maxY - 740))
-        makeLevelNode6(yPoint: Float(frame.maxY - 900))
-        makeLevelNode7(yPoint: Float(frame.maxY - 1060))
+        makeLevelNode1(yPoint: Float(frame.maxY - 190))
+        makeLevelNode2(yPoint: Float(frame.maxY - 350))
+        makeLevelNode3(yPoint: Float(frame.maxY - 510))
+        makeLevelNode4(yPoint: Float(frame.maxY - 670))
+        makeLevelNode5(yPoint: Float(frame.maxY - 830))
+        makeLevelNode6(yPoint: Float(frame.maxY - 990))
+        makeLevelNode7(yPoint: Float(frame.maxY - 1150))
     }
     
     
@@ -42,7 +42,7 @@ class LevelSelectScene: SKScene
     {
         levelNode1 = SKSpriteNode(texture: SKTexture(imageNamed: "1"))
         levelNode1.size =  CGSize(width: 300, height: 300)
-        levelNode1.position = CGPoint(x: frame.midX, y: CGFloat(yPoint))
+        levelNode1.position = CGPoint(x: frame.midX + 80, y: CGFloat(yPoint))
         levelNode1.zPosition = 1
         levelNode1.name = "1"
         addChild(levelNode1)
@@ -52,53 +52,58 @@ class LevelSelectScene: SKScene
     {
         levelNode2 = SKSpriteNode(texture: SKTexture(imageNamed: "2"))
         levelNode2.size =  CGSize(width: 300, height: 300)
-        levelNode2.position = CGPoint(x: frame.midX, y: CGFloat(yPoint))
-        levelNode2.zPosition = 1
+        levelNode2.position = CGPoint(x: frame.midX + 80, y: CGFloat(yPoint))
+        levelNode2.zPosition = 2
         levelNode2.name = "2"
         addChild(levelNode2)
     }
     
     func makeLevelNode3(yPoint: Float)
     {
-        levelNode3 = SKSpriteNode(color: UIColor.yellow, size: (CGSize(width: 150, height: 150)))
-        levelNode3.position = CGPoint(x: frame.midX, y: CGFloat(yPoint))
-        levelNode3.zPosition = 1
+        levelNode3 = SKSpriteNode(texture: SKTexture(imageNamed: "3"))
+        levelNode3.size =  CGSize(width: 300, height: 300)
+        levelNode3.position = CGPoint(x: frame.midX + 80, y: CGFloat(yPoint))
+        levelNode3.zPosition = 3
         levelNode3.name = "3"
         addChild(levelNode3)
     }
     
     func makeLevelNode4(yPoint: Float)
     {
-        levelNode4 = SKSpriteNode(color: UIColor.yellow, size: (CGSize(width: 150, height: 150)))
-        levelNode4.position = CGPoint(x: frame.midX, y: CGFloat(yPoint))
-        levelNode4.zPosition = 1
+        levelNode4 = SKSpriteNode(texture: SKTexture(imageNamed: "4"))
+        levelNode4.size =  CGSize(width: 300, height: 300)
+        levelNode4.position = CGPoint(x: frame.midX + 80, y: CGFloat(yPoint))
+        levelNode4.zPosition = 4
         levelNode4.name = "4"
         addChild(levelNode4)
     }
     
     func makeLevelNode5(yPoint: Float)
     {
-        levelNode5 = SKSpriteNode(color: UIColor.yellow, size: (CGSize(width: 150, height: 150)))
-        levelNode5.position = CGPoint(x: frame.midX, y: CGFloat(yPoint))
-        levelNode5.zPosition = 1
+        levelNode5 = SKSpriteNode(texture: SKTexture(imageNamed: "5"))
+        levelNode5.size =  CGSize(width: 300, height: 300)
+        levelNode5.position = CGPoint(x: frame.midX + 80, y: CGFloat(yPoint))
+        levelNode5.zPosition = 5
         levelNode5.name = "5"
         addChild(levelNode5)
     }
     
     func makeLevelNode6(yPoint: Float)
     {
-        levelNode6 = SKSpriteNode(color: UIColor.yellow, size: (CGSize(width: 150, height: 150)))
-        levelNode6.position = CGPoint(x: frame.midX, y: CGFloat(yPoint))
-        levelNode6.zPosition = 1
+        levelNode6 = SKSpriteNode(texture: SKTexture(imageNamed: "6"))
+        levelNode6.size =  CGSize(width: 300, height: 300)
+        levelNode6.position = CGPoint(x: frame.midX + 80, y: CGFloat(yPoint))
+        levelNode6.zPosition = 6
         levelNode6.name = "6"
         addChild(levelNode6)
     }
     
     func makeLevelNode7(yPoint: Float)
     {
-        levelNode7 = SKSpriteNode(color: UIColor.yellow, size: (CGSize(width: 150, height: 150)))
-        levelNode7.position = CGPoint(x: frame.midX, y: CGFloat(yPoint))
-        levelNode7.zPosition = 1
+        levelNode7 = SKSpriteNode(texture: SKTexture(imageNamed: "7"))
+        levelNode7.size =  CGSize(width: 300, height: 300)
+        levelNode7.position = CGPoint(x: frame.midX + 80, y: CGFloat(yPoint))
+        levelNode7.zPosition = 7
         levelNode7.name = "7"
         addChild(levelNode7)
     }
